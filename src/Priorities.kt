5ae58c09dd53c24d3cee3bc49da9682d2d83375f
aqilusman45 <aqilusman45@gmail.com>
2019-10-20 23:32:05 +0500

    Priorities

---
+++ src/Priorities.kt
@@ -0,0 +1,17 @@
+fun main(){
+    // Priorities while performing mathematical operations
+    // Just like in maths Kotlin follows BODMAS rule
+    // which is as follows
+    // 1- ()
+    // 2- ^
+    // 3- *, /
+    // 4- +, -
+    // 5- =
+    var n1 = 20;
+    var n2 =30;
+    var n3 = 4;
+    var n4 = 3;
+    var exp: Int?;
+    exp = n1+n4 * n3 - n2;
+    println(exp);
+}
